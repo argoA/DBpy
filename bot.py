f96ca5bad507bd584b0e9cbe154dfe2733059ed9
argoa <austin.argo@yahoo.com>
2020-08-14 22:01:54 -0600

    basic admin commands setup

--- bot.py
+++ bot.py
@@ -15,26 +15,31 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
 	print(f'{member} has joined a server.')
-	await member.create_dm()
-	await member.dm_channel.send(
-		f'Hi there {member.name}, welcome to the server!'
-	)
 
 @bot.event
 async def on_member_remove(member):
 	print(f'{member} has left a server.')
 
 # COGS
-@bot.command()
+@bot.command(help='Loads a specific cog')
 async def load(ctx, extension):
 	bot.load_extension(f'cogs.{extension}')
 
-@bot.command()
+@bot.command(help='Unloads a specific cog')
 async def unload(ctx, extension):
 	bot.unload_extension(f'cogs.{extension}')
 
+# Loads all local cogs
 for cog in os.listdir('./cogs'):
 	if cog.endswith('.py'):
 		bot.load_extension(f'cogs.{cog[:-3]}')
 
-bot.run(TOKEN)
+# Made it this far. Time to start the bot
+try:
+	bot.loop.run_until_complete(bot.start(TOKEN))
+except KeyboardInterrupt:
+	pass
+finally:
+	bot.loop.run_until_complete(bot.logout())
+
+	bot.loop.close()
--- cogs/admin-commands.py
+++ cogs/admin-commands.py
@@ -6,27 +6,39 @@ class Admin(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(help='checks server ping')
+	@commands.command(help='Checks the server ping')
 	@checks.is_server_admin()
 	async def ping(self, ctx):
 		await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
 
-	@commands.command()
+
+	@commands.command(help='Deletes a user\'s message(s)', usage='<limit> [-v]')
+	@checks.is_server_admin()
+	async def delete(self, ctx, member : discord.Member, limit=5, verbose=''):
+		deleted = 0
+		async for message in ctx.channel.history(limit=int(limit)):
+			if message.author == member:
+				await message.delete()
+				deleted += 1
+		if verbose == '-v':
+			await ctx.send(f'{deleted} of {member.name} messages!')
+
+	@commands.command(help='Purges current channels messages', usage='[limit]')
 	@checks.is_server_admin()
 	async def clear(self, ctx, amount=5):
 		await ctx.channel.purge(limit=amount)
 
-	@commands.command()
+	@commands.command(help='Kicks a member')
 	@checks.is_server_admin()
 	async def kick(self, ctx, member : discord.Member, *, reason=None):
 		await member.kick(reason=reason)
 
-	@commands.command()
+	@commands.command(help='Bans a member')
 	@checks.is_server_admin()
 	async def ban(self, ctx, member : discord.Member, *, reason=None):
 		await member.ban(reason=reason)
 
-	@commands.command()
+	@commands.command(help='Unbans a user')
 	@checks.is_server_admin()
 	async def unban(self, ctx, member):
 		banned_users = await ctx.guild.bans()
